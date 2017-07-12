047908c692209952aba289f024913cc0fb88178f
Gennadiy Balachkov <bigboxforfox@gmail.com>
2017-07-12 11:17:53 +0300

    change line by polifil

--- front/src/components/tournament/grid/Bracket.vue
+++ front/src/components/tournament/grid/Bracket.vue
@@ -3,26 +3,12 @@
     :style="this.svgPosition"
     :id="`svg-round${roundId}-team${teamId}`"
     )
-    line(
-      v-for="line in svgLine"
-      v-if="havePreviousGame()"
-      :x1="line.x1"
-      :y1="line.y1"
-      :x2="line.x2"
-      :y2="line.y2"
+    polyline(
+      @click="getSvgPolifil"
+      :points="getSvgPolifil()"
+      fill="rgb(249,249,249)"
       stroke-width="1"
-      stroke="rgb(0,0,0)"
-      )
-    line(
-      v-for="line in svgZeroLine"
-      v-if="!havePreviousGame()"
-      :x1="line.x1"
-      :y1="line.y1"
-      :x2="line.x2"
-      :y2="line.y2"
-      stroke-width="1"
-      stroke="rgb(0,0,0)"
-      )
+      stroke="rgb(0,0,0)")
 </template>
 
 <script>
@@ -31,8 +17,7 @@ export default {
   data () {
     return {
       svgPosition: {},
-      svgLine: [],
-      svgZeroLine: []
+      svgPolifil: []
     }
   },
   mounted () {
@@ -46,6 +31,13 @@ export default {
     }
   },
   methods: {
+    getSvgPolifil () {
+      let coordinates = ''
+      this.svgPolifil.forEach(point => {
+        coordinates += `${point.x},${point.y} `
+      })
+      return coordinates
+    },
     havePreviousGame () {
       let roundId = this.roundId
       let teamId = this.teamId
@@ -81,24 +73,22 @@ export default {
             top: from.top + from.height / 2,
             height: to.top - from.top - to.height / 2
           }
-          this.svgLine = [
+          this.svgPolifil = [
             {
-              x1: 0,
-              y1: 0,
-              x2: this.svgPosition.width / 2,
-              y2: 0
+              x: 0,
+              y: 0
             },
             {
-              x1: this.svgPosition.width / 2,
-              y1: 0,
-              x2: this.svgPosition.width / 2,
-              y2: this.svgPosition.height
+              x: this.svgPosition.width / 2,
+              y: 0
             },
             {
-              x1: this.svgPosition.width / 2,
-              y1: this.svgPosition.height,
-              x2: this.svgPosition.width,
-              y2: this.svgPosition.height
+              x: this.svgPosition.width / 2,
+              y: this.svgPosition.height
+            },
+            {
+              x: this.svgPosition.width,
+              y: this.svgPosition.height
             }
           ]
         } else {
@@ -109,24 +99,22 @@ export default {
             top: to.top + to.height / 2,
             height: from.top - to.top + from.height / 4
           }
-          this.svgLine = [
+          this.svgPolifil = [
             {
-              x1: 0,
-              y1: this.svgPosition.height,
-              x2: this.svgPosition.width / 2,
-              y2: this.svgPosition.height
+              x: 0,
+              y: this.svgPosition.height
             },
             {
-              x1: this.svgPosition.width / 2,
-              y1: this.svgPosition.height,
-              x2: this.svgPosition.width / 2,
-              y2: 0
+              x: this.svgPosition.width / 2,
+              y: this.svgPosition.height
             },
             {
-              x1: this.svgPosition.width / 2,
-              y1: 0,
-              x2: this.svgPosition.width,
-              y2: 0
+              x: this.svgPosition.width / 2,
+              y: 0
+            },
+            {
+              x: this.svgPosition.width,
+              y: 0
             }
           ]
         }
@@ -138,18 +126,18 @@ export default {
           width: 15,
           height: 15
         }
-        this.svgZeroLine = [
+        this.svgPolifil = [
           {
-            x1: 0,
-            y1: 0,
-            x2: 0,
-            y2: this.svgPosition.height
+            x: 0,
+            y: 0
           },
           {
-            x1: 0,
-            y1: this.svgPosition.height,
-            x2: this.svgPosition.width,
-            y2: this.svgPosition.height
+            x: 0,
+            y: this.svgPosition.height
+          },
+          {
+            x: this.svgPosition.width,
+            y: this.svgPosition.height
           }
         ]
       }
